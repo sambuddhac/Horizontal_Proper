b926d5ebc5a9324cb219d808dc3ad75b9442be01
hosnakhajeh <66817492+hosnakhajeh@users.noreply.github.com>
2021-02-20 12:31:59 +0200

    Update nettran.py
    Modified the translation from line 1 to 212

--- CentralControl_PyJuMP/Python_src/nettran.py
+++ CentralControl_PyJuMP/Python_src/nettran.py
@@ -66,160 +66,149 @@ class Nettran(object):
 			#end initialization for Nodes
 
 ############################################################################################/* Generators */
-        matrixGen = json.load(open(os.path.join("data", self.genFile))) #ifstream constructor opens the file of Generators
-		j = 0 #counter for generators
-		for matrixGenFile in matrixGen:
-			gNodeID = matrixGenFile['genNodeID'] #node object ID to which the particular generator object is connected
+        		matrixGen = json.load(open(os.path.join("data", self.genFile))) #ifstream constructor opens the file of Generators
+			j = 0 #counter for generators
+			for matrixGenFile in matrixGen:
+				gNodeID = matrixGenFile['genNodeID'] #node object ID to which the particular generator object is connected
 			#log.info("\nConnection Node defined.\n")
 			#Parameters for Generator
 			#Quadratic Coefficient: 
-			c2 = matrixGenFile['quadCostCoeff'] * (100**2)
+				c2 = matrixGenFile['quadCostCoeff'] * (100**2)
 			#Linear coefficient: 
-			c1 = matrixGenFile['linCostCoeff'] * 100
+				c1 = matrixGenFile['linCostCoeff'] * 100
 			#Constant term: 
-			c0 = matrixGenFile['noLoadCost']
+				c0 = matrixGenFile['noLoadCost']
 			#Maximum Limit: 
-			PgMax = matrixGenFile['PgMax'] / 100
+				PgMax = matrixGenFile['PgMax'] / 100
 			#Minimum Limit: 
-			PgMin = matrixGenFile['PgMin'] / 100
+				PgMin = matrixGenFile['PgMin'] / 100
 			#/* Secant Approximation of the Quadratic Cost curve */
 			#Tangent Ratio of the secant approximation of the intercepted cost curve
-			tanTheta = (c2*(PgMax**2)+c1*PgMax-c2*(PgMin**2)-c1*PgMin)/(PgMax-PgMin)
-			minCost = c2*(PgMin**2)+c1*PgMin+c0-tanTheta*PgMin
+				tanTheta = (c2*(PgMax**2)+c1*PgMax-c2*(PgMin**2)-c1*PgMin)/(PgMax-PgMin)
+				minCost = c2*(PgMin**2)+c1*PgMin+c0-tanTheta*PgMin
 			#Intercept value or cost at minimum power level
 			#check the bounds and validity of the parameter values
-			genInstance = Powergenerator(j+1, self.nodeObject[ gNodeID - 1 ],  tanTheta, minCost, PgMax, PgMin)
-			self.genObject.append(genInstance) #push the generator object into the array
-			j +=1 #increment counter
-		matrixGen.close() #Close the generator file
-		self.genDF = pd.DataFrame([g.__dict__ for g in self.genObject])
+				genInstance = self.Powergenerator( univGenNum + j+1, nodeObject[ univNodeNum + gNodeID - 1 ],  tanTheta, minCost, PgMax, PgMin )
+				self.genObject.append(genInstance) #push the generator object into the array
+				j +=1 #increment counter
+			matrixGen.close() #Close the generator file
+			self.genDF = pd.DataFrame([g.__dict__ for g in self.genObject])
 	######################################################################################			#/* Transmission Lines */
-        matrixTranFile = json.load(open(os.path.join("data", self.tranFile))) #ifstream constructor opens the file of Transmission lines
+        		if self.tranNumber > 0:
+				matrixTranFile = json.load(open(os.path.join("data", self.tranFile))) #ifstream constructor opens the file of Transmission lines
 		#/* Instantiate Transmission Lines */
-		k=0 #counter for transmission lines
-		for matrixTran in matrixTranFile:
+				k=0 #counter for transmission lines
+				for matrixTran in matrixTranFile:
 			#node IDs of the node objects to which this transmission line is connected.
-			tNodeID1 = matrixTran['fromNode'] #From end
-			tNodeID2 = matrixTran['toNode'] #To end
+					tNodeID1 = matrixTran['fromNode'] #From end
+					tNodeID2 = matrixTran['toNode'] #To end
 			#Reactance
-			reacT = matrixTran['Reactance']
+					reacT = matrixTran['Reactance']
 			#values of maximum allowable power flow on line in the forward and reverse direction:
-			ptMax = matrixTran['lineLimit']/100
+					ptMax = matrixTran['lineLimit']/100
 			#creates transLineInstance object with ID k + 1
-			transLineInstance = transmissionLine(k + 1, self.nodeObject[ tNodeID1 - 1 ], self.nodeObject[ tNodeID2 - 1 ], ptMax, reacT) 
-			self.translObject.append( transLineInstance ) #pushes the transLineInstance object into the vector
-			k +=1 #increment the counter
+					transLineInstance = self.transmissionLine( univTranNum + k + 1, self.nodeObject[ univNodeNum + tNodeID1 - 1 ], self.nodeObject[ univNodeNum + tNodeID2 - 1 ], ptMax, reacT ) 
+					self.translObject.append( transLineInstance ) #pushes the transLineInstance object into the vector
+					k +=1 #increment the counter
 		#end initialization for Transmission Lines 
-		matrixTranFile.close() #Close the transmission line file 
-		self.tranDF = pd.DataFrame([t.__dict__ for t in self.translObject])
+				matrixTranFile.close() #Close the transmission line file 
+				self.tranDF = pd.DataFrame([t.__dict__ for t in self.translObject])
     ######################################################################################/* Internal Candidate Transmission Lines */
-        matrixIntCETranFile = json.load(open(os.path.join("data", self.intCandLineFile))) #ifstream constructor opens the file of internal candidate Transmission lines
-        k1 = 0 #Counter for internal candidate lines
+			if self.internalCLines > 0:
+        			matrixIntCETranFile = json.load(open(os.path.join("data", self.intCandLineFile))) #ifstream constructor opens the file of internal candidate Transmission lines
+        			k = 0 #Counter for internal candidate lines
 			#/* Instantiate Internal Candidate Transmission Lines */
-		for matrixIntCETran in matrixIntCETranFile:
+				for matrixIntCETran in matrixIntCETranFile:
 				#node object IDs to which the particular transmission line object is connected
 				#node IDs of the node objects to which this transmission line is connected.
-			tNodeID1 = matrixIntCETran['fromNode'] #From end node 
-			tNodeID2 = matrixIntCETran['toNode'] #To end node
+					tNodeID1 = matrixIntCETran['fromNode'] #From end node 
+					tNodeID2 = matrixIntCETran['toNode'] #To end node
 				#Parameters for Transmission Line
 				#Reactance:
-			reacT = matrixIntCETran['Reactance']
+					reacT = matrixIntCETran['Reactance']
 				#values of maximum allowable power flow on line in the forward and reverse direction:
 				#Forward direction:
-			ptMax = matrixIntCETran['lineLimit']/100
-			lifeTime = matrixIntCETran['lifeTime'] #life time of the candidate line
-			interestRate = matrixIntCETran['interestRate'] #interest rate of the investment 
-			costPerCap = matrixIntCETran['costPerCap']*ptMax #capital cost for the construction 
-			presAbsence = matrixIntCETran['presAbsence'] #status of the construction
+					ptMax = matrixIntCETran['lineLimit']/100
+					lifeTime = matrixIntCETran['lifeTime'] #life time of the candidate line
+					interestRate = matrixIntCETran['interestRate'] #interest rate of the investment 
+					costPerCap = matrixIntCETran['costPerCap']*ptMax #capital cost for the construction 
+					presAbsence = matrixIntCETran['presAbsence'] #status of the construction
 			
 				#creates intCandLineInstance object with ID k + 1
-			intCandLineInstance = intCandLine( k1 + 1, self.nodeObject[ tNodeID1 - 1 ], self.nodeObject[ tNodeID2 - 1 ], ptMax, reacT, interestRate, lifeTime, costPerCap, presAbsence ) #Create the internal candidate transmission line object with node 1 
-			self.intCandLineObject.append( intCandLineInstance ) #pushes the transLineInstance object into the vector
-            k2 +=1 #increment the counter
+					intCandLineInstance = self.intCandLine( univIntCandNum + k + 1, self.nodeObject[ univNodeNum + tNodeID1 - 1 ], self.nodeObject[ univNodeNum + tNodeID2 - 1 ], ptMax, reacT, interestRate, lifeTime, costPerCap, presAbsence ) #Create the internal candidate transmission line object with node 1 
+					self.intCandLineObject.append( intCandLineInstance ) #pushes the transLineInstance object into the vector
+            				k +=1 #increment the counter
 			#end initialization for candidate Transmission Lines
-		matrixIntCETranFile.close() #Close the candidate lines file
+				matrixIntCETranFile.close() #Close the candidate lines file
+				self.intCandtranDF = pd.DataFrame([t.__dict__ for t in elf.intCandLineObject])
 ################################################################################# #/* Loads */
-        matrixLoadFile = json.load(open(os.path.join("data", self.loadFile))) #ifstream constructor opens the file of Loads
-		for l in range(self.nodeNumber):
-		    (self.nodeObject[l]).initLoad(self.countOfScenarios) #Initialize the default loads on all nodes to zero
+        		matrixLoadFile = json.load(open(os.path.join("data", self.loadFile))) #ifstream constructor opens the file of Loads
+			countOfScenarios = loadFields-1
+			for l in range(self.nodeNumber+self.nodeNumber):
+		    		(self.nodeObject[l]).initLoad(self.countOfScenarios) #Initialize the default loads on all nodes to zero
 		#end initialization for Nodes		
 		#/* Instantiate Loads */
-		for matrixLoad in matrixLoadFile:
+			for j in loadNumber:
+				for matrixLoad in matrixLoadFile:
 			#node object ID to which the particular load object is connected
 			#node ID of the node object to which this load object is connected.
-			lNodeID = matrixLoad['loadNodeID']
-			for f in range(self.countOfScenarios):
+					lNodeID = matrixLoad['loadNodeID']
+					for f in range(self.countOfScenarios):
 				#value of allowable power consumption capability of load in pu with a negative sign to indicate consumption:
 				#Power Consumption:
-				P_Load[f] = matrixLoad['loadMW']/100
+						P_Load[f] = matrixLoad['loadMW']/100
 
-			loadInstance = Load( j + 1, self.nodeObject[ lNodeID - 1 ], loadFields-1, P_Load ) #creates loadInstance object object with ID number j + 1
+				loadInstance = self.Load( univLoadNum + j + 1, self.nodeObject[ univNodeNum + lNodeID - 1 ], loadFields-1, P_Load ) #creates loadInstance object object with ID number j + 1
 
-			self.loadObject.append( loadInstance ) #pushes the loadInstance object into the vector
+				self.loadObject.append( loadInstance ) #pushes the loadInstance object into the vector
 		#end initialization for Loads
-		matrixLoadFile.close() #Closes the load file
-		for f in range(self.countOfScenarios):
-			self.probability.append(1/(self.countOfScenarios))
-############################################################################################ Can't underestand what are these codes doing
-			
-		univNodeNum += nodeNumber #Increment the universal node number
+			matrixLoadFile.close() #Closes the load file
+			for f in range(self.countOfScenarios):
+				self.probability.append(1/(self.countOfScenarios))
+########################################################################################################################	
+		univNodeNum = univNodeNum + nodeNumber #Increment the universal node number
 		nodeNumVector.append(nodeNumber)
-		univGenNum += genNumber #Increment the universal generator number
-		univTranNum += tranNumber #Increment the universal transmission line number
-		univLoadNum += loadNumber #Increment the universal load number
-		univIntCandNum += internalCLines #Increment the universal intra zonal candidate line number
-###################################################################################################### 
-""" I don't know what are the following codes doing...
-	}
-	for ( int zonalIndex = 1; zonalIndex <= zonalCount; ++zonalIndex ) { // Iterate through the zones 
-		strcpy( netFile, initSummary[(zonalIndex-1)].c_str() ); // String for storing the name of the network file
-		strcpy( sharedLineFile, initSummary[(zonalIndex-1)+2*zoneCount].c_str() ); // String for storing the name of the shared existing lines file
-		strcpy( candLineFile, initSummary[(zonalIndex-1)+5*zoneCount].c_str() ); // String for storing the name of the candidate lines file
-		do {
-			ifstream matrixNetFile( netFile, ios::in ); // ifstream constructor opens the file of Network	
-			// exit program if ifstream could not open file
-			if ( !matrixNetFile ) {
-				cerr << "\nFile for Network could not be opened\n" << endl;
-				exit( 1 );
-			} // end if
-			matrixNetFile >> nodeNumber >> sharedELines >> sharedCLines >> genNumber >> loadNumber >> tranNumber >> internalCLines; // get the dimensions of the Network
-			/* Shared Existing Transmission Lines */
-			ifstream matrixSETranFile( sharedLineFile, ios::in ); // ifstream constructor opens the file of Transmission lines
-"""
+		univGenNum = univGenNum + genNumber #Increment the universal generator number
+		univTranNum = univTranNum + tranNumber #Increment the universal transmission line number
+		univLoadNum = univLoadNum + loadNumber #Increment the universal load number
+		univIntCandNum = univIntCandNum + internalCLines #Increment the universal intra zonal candidate line number
+######################################################################################################################## 
+		for zonalIndex in jSONList: #Iterate through the zones 
+			self.netFile = zonalIndex['Network File'] 
+			self.sharedLineFile = jSONIndex['Shared Lines File'] 
+			self.tranFile = zonalIndex['Transmission Lines File'] 
+			self.candLineFile = jSONIndex['Candidate Lines File'] 
 ########################################################################################### Shared Existing Transmission lines
-		matrixSETranFile = json.load(open(os.path.join("data", self.sharedLineFile))) #ifstream constructor opens the file of Transmission lines
-		H1 = 0
+			matrixSETranFile = json.load(open(os.path.join("data", self.sharedLineFile))) #ifstream constructor opens the file of Transmission lines
+			k = 0
 		#/* Instantiate Shared Existing Transmission Lines */
-		for matrixSETran in matrixSETranFile:
+			for matrixSETran in matrixSETranFile:
 			#log.info("Tran File Test Message 1 from line {} before creation1".format(k))
 			#node IDs of the node objects to which this transmission line is connected.
-			serNum = matrixSETran['globalSerial'] #global serial number of the shared existing transmission line
-			tNodeID1 = matrixSETran['fromNode'] #From end node 
-			nodeZone1 = matrixSETran['fromZone'] #From end zone number
-			tNodeID2 = matrixSETran['toNode'] #To end node
-			nodeZone2 = matrixSETran['toZone'] #To end zone number
+				serNum = matrixSETran['globalSerial'] #global serial number of the shared existing transmission line
+				tNodeID1 = matrixSETran['fromNode'] #From end node 
+				nodeZone1 = matrixSETran['fromZone'] #From end zone number
+				tNodeID2 = matrixSETran['toNode'] #To end node
+				nodeZone2 = matrixSETran['toZone'] #To end zone number
 			#Reactance:
-			reacT = matrixSETran['Reactance']
+				reacT = matrixSETran['Reactance']
 			#values of maximum allowable power flow on line in the forward and reverse direction:
-			ptMax = matrixSETran['lineLimit']/100
-			SELineInstance = SELine(H1 + 1, serNum, self.nodeObject[ tNodeID1 - 1 ], tNodeID1, nodeZone1, tNodeID2, nodeZone2, self.zonalIndex, ptMax, reacT) #Create the shared existing transmission line object with node 1 
-			self.SELineObject.append(SELineInstance) #pushes the transLineInstance object into the vector
-			H1 +=1
-		matrixSETranFile.close() #Close the shared existing file
-		self.sharedExistingDF = pd.DataFrame([seDF.__dict__ for seDF in self.SELineObject])
-	""" I didn't translate the following codes because I can't get what are they going to do????
-	// Renumbering the node IDs with the aggregated count
-					int newNodeBase = 0; // 
-					for (int aggrCount = 0; aggrCount < nodeZone1; ++aggrCount) {
-						newNodeBase+=nodeNumVector[aggrCount];
-					}
-					tNodeID1 += newNodeBase;
-					newNodeBase = 0; // 
-					for (int aggrCount = 0; aggrCount < nodeZone2; ++aggrCount) {
-						newNodeBase+=nodeNumVector[aggrCount];
-					}
-					tNodeID2 += newNodeBase;
-	"""
+				ptMax = matrixSETran['lineLimit']/100
+				if serNum in SELineSerList:
+					newNodeBase = 0
+					for aggrCount in nodeZone1:
+						newNodeBase = newNodeBase + nodeNumVector[aggrCount]
+					tNodeID1 = tNodeID1 + newNodeBase
+					newNodeBase = 0
+					for aggrCount in nodeZone2:
+						newNodeBase = newNodeBase + nodeNumVector[aggrCount]
+					tNodeID2 = tNodeID2 + newNodeBase
+					SELineInstance = self.SELine( k + 1, serNum, self.nodeObject[ tNodeID1 - 1 ], self.nodeObject[ tNodeID2 - 1 ], ptMax, reacT )
+					SELineObject.append( SELineInstance )
+					SELineSerList.append(serNum)
+					univSELineNum = univSELineNum + 1
+			matrixSETranFile.close() #Close the shared existing file
+			self.sharedExistingDF = pd.DataFrame([seDF.__dict__ for seDF in self.SELineObject])
 ###########################################################################################/* Shared Candidate Transmission Lines */
         matrixCETranFile = json.load(open(os.path.join("data", self.candLineFile))) #ifstream constructor opens the file of candidate Transmission lines
 		#/* Instantiate Shared Candidate Transmission Lines */
