a911163574ebc1d3e9cf2dbe129154b9edeb9434
hosnakhajeh <66817492+hosnakhajeh@users.noreply.github.com>
2021-06-25 16:29:46 +0300

    Update powergenerator.py
    change functions' names + review

--- CentralControl_PyJuMP/Python_src/powergenerator.py
+++ CentralControl_PyJuMP/Python_src/powergenerator.py
@@ -3,42 +3,42 @@ from Python_src.node import Node
 
 #constructor definition for the piecewise linear objective function
 class Powergenerator(object):
-	def __init__(self, ID, nodeConng, incCost, noLoad, max, Min):
-		self.genID = ID #Initializer list to initialize data members that don't need validity check
-		self.connNodegPtr = nodeConng
-		self.noLoadCost = noLoad
-		self.incrementalCost = incCost #Initialize this data member to zero (unused for piecewise linear objective)
-		self.setGenParamsSimple(max, Min) #call the set function to perform validity check on parameter value ranges and assign the values 
-		self.connNodegPtr.setgConn(self.genID) #increments the generation connection variable to node
+	def __init__(self, _id, node_conng, inc_cost, no_load, max, min):
+		self.gen_id = _id #Initializer list to initialize data members that don't need validity check
+		self.conn_nodeg_ptr = node_conng
+		self.no_load_cost = no_load
+		self.incremental_cost = inc_cost #Initialize this data member to zero (unused for piecewise linear objective)
+		self.set_gen_params_simple(max, min) #call the set function to perform validity check on parameter value ranges and assign the values 
+		self.conn_nodeg_ptr.setg_conn(self.gen_id) #increments the generation connection variable to node
 		#end of constructor
 
 
-	def setGenParamsSimple(self, max, Min): #set function to set the Powergenerator class data members min max limits
-		self.pMax = max
-		self.pMin = Min 
-		#end of setGenParams function
+	def set_gen_params_simple(self, max, min): #set function to set the Powergenerator class data members min max limits
+		self.p_max = max
+		self.pmin = min 
+		#end of set_gen_params function
 
 	def __del__(): #destructor definition
-		#log.info("\nGenerator object {}  destroyed".format(self.genID))
+		#log.info("\n_generator object {}  destroyed".format(self.gen_id))
 		#end of destructor
 
-	def getGenID(self): #returns the Powergenerator ID number
-		return self.genID
-		#end of getGenID function
+	def get_gen_id(self): #returns the Powergenerator _id number
+		return self.gen_id
+		#end of get_gen_id function
 
-	def getPMax(self): #function getPMax begins
-		return self.pMax
-		#getPMax ends
+	def get_p_max(self): #function get_p_max begins
+		return self.p_max
+		#get_p_max ends
 
-	def getPMin(self): #function getPMin begins
-		return self.pMin
-		#getPMax ends
+	def get_pmin(self): #function get_pmin begins
+		return self.pmin
+		#get_p_max ends
 
-	def getLinCoeff(self): #Gets the linear coefficient (Incremental production cost
-		return self.incrementalCost
+	def get_line_coeff(self): #Gets the linear coefficient (Incremental production cost
+		return self.incremental_cost
 		#function getLinCoeff ends
 
-	def getNLCost(self): #Gets the no load cost
-		return self.noLoadCost
+	def get_nl_cost(self): #Gets the no load cost
+		return self.no_load_cost
 		#function getNLCost ends 
 
