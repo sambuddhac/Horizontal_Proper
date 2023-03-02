641fe9f6ac9d61c89fecf6bc5bf5ad5050968ca7
nudes <26635592+Tom-Nudell@users.noreply.github.com>
2023-03-02 11:34:42 -0800

    Modifications centralized doc (#28)
    * streamlining julenv and activate files
    
    * updating first few cells that introduce notebook and activate packages; there is still some room for improvement here
    
    * updated gitignore
    
    * sorted out scen_weight and zone CSV issues. added note that network data are converted xlsx files. updated documentation around JuMP problem def
    
    * changed all instances of Mod3 to Mod since this is the only model we use in the notebook
    
    * commented and cleaned up objective JuMP definition code
    
    * fixed formatted issue with CSV that I had previously introduced with additional white spaces in the table
    
    ---------
    
    Co-authored-by: Tasteful Nudes <nudes@mq13.lan>

--- central_control_PyJuMP/Julia_src/activate.jl
+++ central_control_PyJuMP/Julia_src/activate.jl
@@ -1,5 +1,4 @@
 
-using Pkg
+# check packages and install if needed
 include(joinpath(dirname(@__FILE__), "julenv.jl")) #Run this line only for the first time; comment it out for all subsequent use
 println("Activating the Julia virtual environment")
-Pkg.status()
